60d2f698cc0369fed18713578cc21cb6df816b69
Sreeharsha Gorantla <Sreeharsha Gorantla>
2016-10-23 02:46:24 -0700

    Modified the path for DBC file

--- Geo/pre_build.sh
+++ Geo/pre_build.sh
@@ -4,4 +4,4 @@
 # This file executes from the build directory by the Eclipse "Pre Build" step
 #
 
-python ../_can_dbc/dbc_parse.py -i C:/SJSU_Dev/projects/CMPE243Project/snf.dbc -s GEO > ../_can_dbc/generated_can.h
+python ../_can_dbc/dbc_parse.py -i C:/SJSU_Dev/projects/spartan_and_furious/snf.dbc -s GEO > ../_can_dbc/generated_can.h
