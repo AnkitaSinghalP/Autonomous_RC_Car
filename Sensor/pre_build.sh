20562c7d36b109aadf08d89c96330e5613e3a0c9
bhushan <muthiyanbhushan@gmail.com>
2016-10-25 16:38:46 -0700

    changed path in pre-build.sh

--- Sensor/pre_build.sh
+++ Sensor/pre_build.sh
@@ -4,4 +4,4 @@
 # This file executes from the build directory by the Eclipse "Pre Build" step
 #
 
-python ../_can_dbc/dbc_parse.py -i ../../spartan_and_furious/snf.dbc -s SENSOR > ../_can_dbc/generated_can.h
+python ../_can_dbc/dbc_parse.py -i ../../snf.dbc -s SENSOR > ../_can_dbc/generated_can.h
