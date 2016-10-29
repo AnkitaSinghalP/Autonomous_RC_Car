75504882c65521251f5eb1bcf2e60957fc26d2d7
Karthik Parameswaran <karthik.parameswaran91@gmail.com>
2016-10-29 14:41:02 -0700

    Commiting basic Usltrasonic sensor code written by Bhushan

--- Sensor/L5_Application/periodic_scheduler/period_callbacks.cpp
+++ Sensor/L5_Application/periodic_scheduler/period_callbacks.cpp
@@ -1,109 +1,314 @@
 /*
+
  *     SocialLedge.com - Copyright (C) 2013
+
  *
+
  *     This file is part of free software framework for embedded processors.
+
  *     You can use it and/or distribute it as long as this copyright header
+
  *     remains unmodified.  The code is free for personal use and requires
+
  *     permission to use in a commercial product.
+
  *
+
  *      THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+
  *      OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+
  *      MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+
  *      I SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR
+
  *      CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+
  *
+
  *     You can reach the author of this software at :
+
  *          p r e e t . w i k i @ g m a i l . c o m
+
  */
+
+
 
 /**
+
  * @file
+
  * This contains the period callback functions for the periodic scheduler
+
  *
+
  * @warning
+
  * These callbacks should be used for hard real-time system, and the priority of these
+
  * tasks are above everything else in the system (above the PRIORITY_CRITICAL).
+
  * The period functions SHOULD NEVER block and SHOULD NEVER run over their time slot.
+
  * For example, the 1000Hz take slot runs periodically every 1ms, and whatever you
+
  * do must be completed within 1ms.  Running over the time slot will reset the system.
+
  */
 
+
+
 #include <stdint.h>
+
 #include "io.hpp"
+
 #include "periodic_callback.h"
+
 #include "io.hpp"
+
 #include "stdio.h"
+
 #include "timers.h"
+
 #include "utilities.h"
+
 #include "file_logger.h"
+
 #include "can.h"
+
 #include "_can_dbc/generated_can.h"
+
 #include "lpc_timers.h"
+
+#include "string.h"
+
+#include "sensor.hpp"
+
+#include "eint.h"
+
 
 
 SENSOR_HEARTBEAT_t sensor_heartbeat_message = {0};
+
 can_msg_t can_msg_sensor = { 0 };
 
+
+
+static int distance_middle =0 ;
+
+static int distance_right =0 ;
+
+static int distance_left =0 ;
+
+static int distance_rear =0 ;
+
+//SYSTEM_CMD_t master_cmd = {0};
+
+
+
+const uint32_t            MASTER_CMD__MIA_MS = 3000;
+
+//const SYSTEM_CMD_t      MASTER_CMD__MIA_MSG = { 25 };
+
+
+
+
+
 /// This is the stack size used for each of the period tasks (1Hz, 10Hz, 100Hz, and 1000Hz)
+
 const uint32_t PERIOD_TASKS_STACK_SIZE_BYTES = (512 * 4);
 
+
+
 /**
+
  * This is the stack size of the dispatcher task that triggers the period tasks to run.
+
  * Minimum 1500 bytes are needed in order to write a debug file if the period tasks overrun.
+
  * This stack size is also used while calling the period_init() and period_reg_tlm(), and if you use
+
  * printf inside these functions, you need about 1500 bytes minimum
+
  */
+
 const uint32_t PERIOD_DISPATCHER_TASK_STACK_SIZE_BYTES = (512 * 3);
 
+
+
 /// Called once before the RTOS is started, this is a good place to initialize things once
-bool period_init(void)
+
+
+
+bool dbc_app_send_can_msg(uint32_t mid, uint8_t dlc, uint8_t bytes[8])
+
 {
-	CAN_init(can1, 100, 128, 256,0,0);
-	CAN_reset_bus(can1);
-	CAN_bypass_filter_accept_all_msgs();
-	return true; // Must return true upon success
+
+	can_msg_t can_msg = { 0 };
+
+	can_msg.msg_id = mid;
+
+	can_msg.frame_fields.data_len = dlc;
+
+	memcpy(can_msg.data.bytes, bytes, dlc);
+
+	return CAN_tx(can1, &can_msg, 0);
+
 }
 
-/// Register any telemetry variables
-bool period_reg_tlm(void)
+
+
+bool period_init(void)
+
 {
-	// Make sure "SYS_CFG_ENABLE_TLM" is enabled at sys_config.h to use Telemetry
+
+	sensor_init();
+
+	CAN_init(can1, 100, 128, 256,0,0);
+
+	CAN_reset_bus(can1);
+
+	CAN_bypass_filter_accept_all_msgs();
+
 	return true; // Must return true upon success
+
 }
+
+
+
+/// Register any telemetry variables
+
+bool period_reg_tlm(void)
+
+{
+
+	// Make sure "SYS_CFG_ENABLE_TLM" is enabled at sys_config.h to use Telemetry
+
+	return true; // Must return true upon success
+
+}
+
+
+
 
 
 /**
+
  * Below are your periodic functions.
+
  * The argument 'count' is the number of times each periodic task is called.
+
  */
 
+
+
 void period_1Hz(uint32_t count)
+
 {
+
 	if(CAN_is_bus_off(can1))
+
 	{
+
 		CAN_reset_bus(can1);
+
 	}
 
+//	if(count % 2 == 0 ){
+
+//		distance_middle =  sensor_sonar_middle_trigger();
+
+//		puts("front sensor");
+
+//	}
+
+//
+
+//	if(count % 3 == 0 ){
+
+//		distance_right = 	sensor_sonar_right_trigger();
+
+//		puts("right sensor");
+
+//	}
+
+//
+
+//
+
+//	if(count % 5 == 0 ){
+
+//		distance_left	= sensor_sonar_left_trigger();
+
+//
+
+//		puts("left sensor");
+
+//	}
+
+//
+
+//
+
+//	if(count % 7 == 0 ){
+
+//		distance_rear =	sensor_sonar_rear_trigger();
+
+//		puts("rear sensor");
+
+//	}
+
+
+
 	LE.toggle(1);
+
 }
+
+
 
 void period_10Hz(uint32_t count)
+
 {
-	sensor_heartbeat_message.SENSOR_HEARTBEAT_tx_bytes = count;
-	sensor_heartbeat_message.SENSOR_HEARTBEAT_rx_bytes = count;
+
+	sensor_heartbeat_message.SENSOR_HEARTBEAT_tx_bytes = 12;
+
+	sensor_heartbeat_message.SENSOR_HEARTBEAT_rx_bytes = 54;
+
 	dbc_encode_and_send_SENSOR_HEARTBEAT(&sensor_heartbeat_message);
 
+
+
+	//puts("sent");
+
+
+
+
+
 	LE.toggle(2);
+
 }
+
+
 
 void period_100Hz(uint32_t count)
+
 {
-	LE.toggle(3);
+
+	//LE.toggle(3);
+
 }
 
+
+
 // 1Khz (1ms) is only run if Periodic Dispatcher was configured to run it at main():
+
 // scheduler_add_task(new periodicSchedulerTask(run_1Khz = true));
+
 void period_1000Hz(uint32_t count)
+
 {
-	LE.toggle(4);
+
+	//LE.toggle(4);
+
 }
+
--- Sensor/L5_Application/sensor.hpp
+++ Sensor/L5_Application/sensor.hpp
@@ -1,31 +1,74 @@
 /*
+
  * sensor.hpp
+
  *
+
  *  Created on: Oct 18, 2016
+
  *      Author: BHUSHAN
+
  */
 
+
+
 #ifndef L5_APPLICATION_SENSOR_HPP_
+
 #define L5_APPLICATION_SENSOR_HPP_
 
+
+
 #include<stdio.h>
+
 #include <stdint.h>
+
 #include "lpc_timers.h"
+
 #include "eint.h"
+
 #include "io.hpp"
+
 #include "gpio.hpp"
+
 #include"timers.h"
+
 #include "utilities.h"
 
+
+
 void sensor_init();
-void isr1();
-void isr2();
-void isr3();
+
+void isr_middle1();
+
+void isr_right1();
+
+void isr_left1();
+
+void isr_rear1();
+
+void isr_middle();
+
+void isr_right();
+
+void isr_left();
+
+void isr_rear();
+
+
+
 int sensor_sonar_middle_trigger();
+
 int sensor_sonar_right_trigger();
+
 int sensor_sonar_left_trigger();
+
 int sensor_sonar_rear_trigger();
-int sensor_sonar_rear_left_trigger();
+
+//int sensor_sonar_rear_left_trigger();
+
+
+
 
 
 #endif /* L5_APPLICATION_SENSOR_HPP_ */
+
