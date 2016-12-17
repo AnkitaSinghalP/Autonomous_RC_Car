826e8979af9ab423909d47cfd2026bc762fa879a
ankita singhal <ankita singhal>
2016-12-17 11:39:06 -0800

    Removed can utilization data

--- Master/L5_Application/periodic_scheduler/heartbeat.cpp
+++ Master/L5_Application/periodic_scheduler/heartbeat.cpp
@@ -2,10 +2,6 @@
 
 void heartbeat_rx(void)
 {
-
-//	uint16_t can_total_bytes = 0;
-//	float bus_util_perc = 0;
-
 	if(dbc_handle_mia_MOTOR_HEARTBEAT(&motor_heartbeat_cmd, 100))
 		system_status_message.MASTER_SYSTEM_STATUS_motor = 0;
 	if(dbc_handle_mia_BLE_HEARTBEAT(&ble_heartbeat_cmd, 100))
@@ -18,12 +14,6 @@ void heartbeat_rx(void)
 		system_status_message.MASTER_SYSTEM_STATUS_io = 0;
 
 	system_status_message.MASTER_SYSTEM_STATUS_master = 1;
-	 /*
-	  * Calculate bus utilization per seconds in percentage to be displayed on LCD. total_bytes * 100 / 100KB
-	  */
-
-//	bus_util_perc = (can_total_bytes)/(1000);
-//	system_status_message.MASTER_SYSTEM_STATUS_util = bus_util_perc;
 
 	dbc_encode_and_send_MASTER_SYSTEM_STATUS(&system_status_message);
 
